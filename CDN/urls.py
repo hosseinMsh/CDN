9a07d2acdd9e748a692cdf4c6801225b192772cd
hossein <h@h.com>
2025-08-19 21:07:10 +0330

    Refactor URL patterns to simplify API endpoint paths and improve code organization in urls.py.

--- CDN/urls.py
+++ CDN/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from accounts.views import RememberLoginView
+from core.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RememberLoginView.as_view(), name='login'),
-    path('', include('core.urls')),
-    path('accounts/',include('accounts.urls'))
-  # private gate
+    path('dashboard/', dashboard, name='dashboard'),
+    path('api/', include('core.urls')),
+    path('accounts/', include('accounts.urls'))
+
 ]
--- core/urls.py
+++ core/urls.py
@@ -3,19 +3,19 @@ from core.views import dashboard, api_upload, api_assets, api_allowed_extensions
     api_mkdir, api_rename, api_delete, api_delete_batch, api_space_set, api_spaces
 
 urlpatterns = [
-    path('dashboard/', dashboard, name='dashboard'),
-    path('api/spaces', api_spaces, name='api_spaces'),
-    path('api/space/set', api_space_set, name='api_space_set'),
 
-    path('api/allowed-extensions', api_allowed_extensions, name='api_allowed_extensions'),
-    path('api/assets', api_assets, name='api_assets'),
-    path('api/browse', api_browse, name='api_browse'),
+    path('spaces', api_spaces, name='api_spaces'),
+    path('space/set', api_space_set, name='api_space_set'),
 
-    path('api/upload', api_upload, name='api_upload'),
-    path('api/zip', api_zip, name='api_zip'),
+    path('allowed-extensions', api_allowed_extensions, name='api_allowed_extensions'),
+    path('assets', api_assets, name='api_assets'),
+    path('browse', api_browse, name='api_browse'),
 
-    path('api/mkdir', api_mkdir, name='api_mkdir'),
-    path('api/rename', api_rename, name='api_rename'),
-    path('api/delete', api_delete, name='api_delete'),
-    path('api/delete-batch', api_delete_batch, name='api_delete_batch'),
+    path('upload', api_upload, name='api_upload'),
+    path('zip', api_zip, name='api_zip'),
+
+    path('mkdir', api_mkdir, name='api_mkdir'),
+    path('rename', api_rename, name='api_rename'),
+    path('delete', api_delete, name='api_delete'),
+    path('delete-batch', api_delete_batch, name='api_delete_batch'),
 ]
