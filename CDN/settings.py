45eb895724dafb57564270938e46ea5502a652f1
hossein <h@h.com>
2025-08-19 23:03:20 +0330

    refactor

--- CDN/settings.py
+++ CDN/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = '/var/www/cdn/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    # '/var/www/cdn/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
