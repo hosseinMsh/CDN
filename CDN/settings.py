0660953e6bb0d8564aa7719727dc0b3817019cbc
hossein <h@h.com>
2025-08-19 21:19:47 +0330

    Add static files directory path to settings.py configuration.

--- CDN/settings.py
+++ CDN/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    '/var/www/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
